0b928dded500f41765eb40d6e3704c175adfd6e4
sengoku kizuku <a15dc095@dhw.ac.jp>
2018-02-25 21:18:40 +0900

    refs create-sp fix

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,48 +1,47 @@
-const gulp					= require('gulp'),
-	bulkSass					= require('gulp-sass-bulk-import'),
-	sass							= require('gulp-sass'),
-	browserSync				= require('browser-sync'), //ブラウザシンク
-	plumber						= require('gulp-plumber'), //エラー通知
-	notify						= require('gulp-notify'), //エラー通知
-	imagemin					= require('gulp-imagemin'), //画像圧縮
-	imageminPngquant	= require('imagemin-pngquant'), //png画像の圧縮
-	pleeease					= require('gulp-pleeease'), //ベンダープレフィックス
-	source						= require('vinyl-source-stream'),
-	browserify				= require('browserify'),
-	babelify					= require('babelify'),
-	browserifyShim		= require('browserify-shim'),
-	watchify					= require('watchify'),
-	useref						= require('gulp-useref'), //ファイル結合
-	gulpif						= require('gulp-if'), // if文
-	uglify						= require('gulp-uglify'), //js圧縮
-	minifyCss					= require('gulp-cssnano'), //css圧縮
-	del								= require('del'), //ディレクトリ削除
-	runSequence				= require('run-sequence'), //並行処理
-	fs								= require('fs'),
-	pug								= require('gulp-pug'), //pug
-	data							= require('gulp-data'), //json-data
-	sourcemaps				= require('gulp-sourcemaps'),
-	jQuery						= require('jquery'),
-	sassGraph					= require('sass-graph'),
-	path							= require('path') //path
-
-
-	const paths = {
-		root: 'dev',
-		public: 'public',
-	}
-	paths.publicImg		= `${path.public}/img`
-	paths.rootImg			= `${path.root}/img`
-	paths.src					=	`${path.root}/src`
-	paths.views				= `${path.src}/views`
-	paths.assets			= `${path.src}/assets`
-	paths.json				= `${paths.views}/_data`
-
+const gulp = require('gulp'),
+	bulkSass = require('gulp-sass-bulk-import'),
+	sass = require('gulp-sass'),
+	browserSync = require('browser-sync'), //ブラウザシンク
+	plumber = require('gulp-plumber'), //エラー通知
+	notify = require('gulp-notify'), //エラー通知
+	imagemin = require('gulp-imagemin'), //画像圧縮
+	imageminPngquant = require('imagemin-pngquant'), //png画像の圧縮
+	pleeease = require('gulp-pleeease'), //ベンダープレフィックス
+	source = require('vinyl-source-stream'),
+	buffer = require('vinyl-buffer'),
+	browserify = require('browserify'),
+	babelify = require('babelify'),
+	browserifyShim = require('browserify-shim'),
+	watchify = require('watchify'),
+	useref = require('gulp-useref'), //ファイル結合
+	gulpif = require('gulp-if'), // if文
+	uglify = require('gulp-uglify'), //js圧縮
+	rename = require('gulp-rename'),
+	minifyCss = require('gulp-cssnano'), //css圧縮
+	del = require('del'), //ディレクトリ削除
+	runSequence = require('run-sequence'), //並行処理
+	fs = require("fs"),
+	pug = require('gulp-pug'), //pug
+	data = require('gulp-data'), //json-data
+	sourcemaps = require('gulp-sourcemaps'),
+	debug = require('gulp-debug'),
+	jQuery = require('jquery'),
+	path = require('path'), //path
+	sassGraph = require('sass-graph'),
+	combineMq = require('gulp-combine-mq'),
+paths = {
+	rootDir: 'dev',
+	dstrootDir: 'public',
+	srcDir: 'dev/img',
+	dstDir: 'public/img',
+	serverDir: 'localhost',
+	json: 'dev/src/views/_data'
+};
 /*
  * Sass
  */
 gulp.task('sass', () => {
-	gulp.src(`${paths.assets}/styles/**/*.scss`)
+	gulp.src(paths.rootDir + '/src/assets/styles/**/*.scss')
 		.pipe(plumber({
 			errorHandler: notify.onError('Error: <%= error.message %>')
 		}))
@@ -51,24 +50,32 @@ gulp.task('sass', () => {
 		.pipe(sass())
 		.pipe(pleeease({
 			sass: true,
-			minifier: true //圧縮の有無 true/false
+			minifier: true
 		}))
 		.pipe(sourcemaps.write('./'))
-		.pipe(gulp.dest(`${paths.root}/css`));
+		.pipe(gulp.dest(paths.rootDir + '/css'));
 });
 
 gulp.task('watch-sass', function(cb) {
 	return watch({
 			glob: './dev/src/assets/styles/**/*scss',
 			emitOnGlob: false,
-			name: 'Sass'
+			name: "Sass"
 		})
 		.pipe(sassGraph(sassLoadPaths))
 		.pipe(sass({
 			loadPath: sassLoadPaths
 		}))
 		.pipe(notify('Sass compiled <%= file.relative %>'))
-		.pipe(gulp.dest(`${paths.root}/css`));
+		.pipe(gulp.dest(paths.rootDir + '/css'));
+});
+
+gulp.task('combineMq',  () =>  {
+	gulp.src(paths.rootDir + './css/common.css')
+		.pipe(combineMq({
+			beautify: false
+		}))
+		.pipe(gulp.dest(paths.rootDir + '/css'));
 });
 
 /*
@@ -76,22 +83,23 @@ gulp.task('watch-sass', function(cb) {
  */
 
 gulp.task('browserify', () => {
+
 	const option = {
 		bundleOption: {
 			cache: {},
 			packageCache: {},
 			fullPaths: false,
-			debug: true,
-			entries: `${paths.assets}/js/main.js`,
+			debug: false,
+			entries: paths.rootDir + '/src/assets/js/main.js',
 			extensions: ['js']
 		},
-		dest: `${paths.root}/js`,
+		dest: paths.rootDir + '/js',
 		filename: 'bundle.js'
 	};
 	const b = browserify(option.bundleOption)
 		.transform(babelify.configure({
-			compact: false,
-			presets: ['es2015']
+			compact: true,
+			presets: ["es2015"]
 		}))
 		.transform(browserifyShim);
 	const bundle = function() {
@@ -113,7 +121,7 @@ gulp.task('browserify', () => {
  * Pleeease
  */
 gulp.task('pleeease', () => {
-	return gulp.src(`${paths.root}/css/*.css`)
+	return gulp.src(paths.rootDir + '/css/*.css')
 		.pipe(pleeease({
 			sass: true,
 			minifier: true //圧縮の有無 true/false
@@ -121,15 +129,15 @@ gulp.task('pleeease', () => {
 		.pipe(plumber({
 			errorHandler: notify.onError('Error: <%= error.message %>')
 		}))
-		.pipe(gulp.dest(`{paths.root}/css`));
+		.pipe(gulp.dest(paths.rootDir + '/css'));
 });
 
 /*
  * Imagemin
  */
 gulp.task('imagemin', () => {
-	const srcGlob = `${paths.rootDirImg}/**/*.+(jpg|jpeg|png|gif|svg)`;
-	const dstGlob = paths.publicImg;
+	const srcGlob = paths.srcDir + '/**/*.+(jpg|jpeg|png|gif|svg)';
+	const dstGlob = paths.dstDir;
 	const imageminOptions = {
 		optimizationLevel: 7,
 		use: imageminPngquant({
@@ -143,38 +151,35 @@ gulp.task('imagemin', () => {
 			errorHandler: notify.onError('Error: <%= error.message %>')
 		}))
 		.pipe(imagemin(imageminOptions))
-		.pipe(gulp.dest(paths.publicImg));
+		.pipe(gulp.dest(paths.dstDir));
 });
 
 /*
  * Useref
  */
 gulp.task('html', () => {
-	return gulp.src(`${paths.root}/**/*.+(html|php)`)
+	return gulp.src(paths.rootDir + '/**/*.+(html|php)')
 		.pipe(useref({
 			searchPath: ['.', 'dev']
 		}))
 		.pipe(gulpif('*.js', uglify()))
 		.pipe(gulpif('*.css', minifyCss()))
-		.pipe(gulp.dest(paths.public));
+		.pipe(gulp.dest(paths.dstrootDir));
 });
 
 /*
  * pug
  */
 gulp.task('pug', () => {
-	gulp.src([
-		`${paths.views}/**/*.pug`,
-		`!${paths.views}/**/_*.pug`
-	])
+	gulp.src([paths.rootDir + '/src/views/**/*.pug', '!' + paths.rootDir + '/src/views/**/_*.pug'])
 		.pipe(plumber({
 			errorHandler: notify.onError('Error: <%= error.message %>')
 		}))
 		.pipe(data(function(file) {
-			const locals = JSON.parse(fs.readFileSync(`${paths.json}/site.json`));
-			locals.relativePath = path.relative(file.base, file.path.replace(/\.pug$/, '.html'));
+			const locals = JSON.parse(fs.readFileSync(paths.json + '/site.json'));
+			locals.relativePath = path.relative(file.base, file.path.replace(/.pug$/, '.html'));
 			return {
-				site: locals
+				'site': locals
 			};
 		}))
 		.pipe(pug({
@@ -182,9 +187,8 @@ gulp.task('pug', () => {
 		}, {
 			ext: '.html'
 		}))
-		.pipe(gulp.dest(paths.root));
+		.pipe(gulp.dest(paths.rootDir));
 });
-
 
 /*
  * Browser-sync
@@ -192,12 +196,11 @@ gulp.task('pug', () => {
 gulp.task('browser-sync', () => {
 	browserSync.init({
 		server: {
-			baseDir: paths.root,
+			baseDir: paths.rootDir,
 			routes: {
-				'/node_modules': 'node_modules'
+				"/node_modules": "node_modules"
 			}
 		},
-		// proxy: 'localhost:8888',
 		notify: true
 	});
 });
@@ -215,34 +218,34 @@ gulp.task('setWatch', () => {
  */
 gulp.task('default', ['browser-sync'], () => {
 	const bsList = [
-		`${paths.root}/**/*.html`,
-		`${paths.root}/**/*.php`,
-		`${paths.root}/js/**/*.js`,
-		`${paths.root}/css/*.css`,
+		paths.rootDir + '/**/*.html',
+		paths.rootDir + '/**/*.php',
+		paths.rootDir + '/js/**/*.js',
+		paths.rootDir + '/css/*.css',
 	];
-	gulp.watch(`${paths.views}/**/*.pug`					, ['pug']);
-	gulp.watch(`${paths.views}/**/*.json`					, ['pug']);
-	gulp.watch(`${paths.assets}/styles/**/*.scss`	, ['sass']);
-	gulp.watch(`${paths.assets}/js/**/*.js`				, ['browserify']);
-	gulp.watch(bsList															, ['bs-reload']);
+	gulp.watch(paths.rootDir + '/src/views/**/*.pug', ['pug']);
+	gulp.watch(paths.rootDir + '/src/assets/styles/**/*.scss', ['sass']);
+	gulp.watch(paths.rootDir + '/src/assets/js/**/*.js', ['browserify']);
+	gulp.watch(paths.rootDir + '/src/views/**/*.json', ['pug']);
+	gulp.watch(bsList, ['bs-reload']);
 });
 
 /*
  * Build
  */
-gulp.task('clean', del.bind(null, [paths.public]));
+gulp.task('clean', del.bind(null, [paths.dstrootDir]));
 gulp.task('devcopy', () => {
 	return gulp.src([
-		`${paths.root}/**/*.*`,
-		// `!${paths.root}/css/**`,
-		// `!${paths.root}/js/**`,
-		`!${paths.views}/**`,
-		`!${paths.assets}/**`,
-		`!${paths.root}/img/**`,
-		`!${paths.root}/**/*.html`,
+		paths.rootDir + '/**/*.*',
+		// '!' + paths.rootDir + '/css/**',
+		// '!' + paths.rootDir + '/js/**',
+		'!' + paths.rootDir + '/src/views/**',
+		'!' + paths.rootDir + '/src/assets/**',
+		'!' + paths.rootDir + '/img/**',
+		'!' + paths.rootDir + '/**/*.html',
 	], {
 		dot: true
-	}).pipe(gulp.dest(paths.public));
+	}).pipe(gulp.dest(paths.dstrootDir));
 });
 gulp.task('build', ['clean'], function(cb) {
 	runSequence('sass', 'browserify', 'pug', ['html', 'imagemin', 'devcopy'], cb);
